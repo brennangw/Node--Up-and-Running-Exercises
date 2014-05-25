555ec47c7cb11fbb845a65d85bd5167912680865
brennangw <brennangoffwallace@gmail.com>
2014-05-25 12:18:22 -0500

    Added non-binary messaging to clients

--- beginningOfANodeChatServer.js
+++ beginningOfANodeChatServer.js
@@ -1,14 +1,16 @@
 var net = require('net')
 
 var chatServer = net.createServer()
+	clientList = []
 
-chatServer.on('connection', function(client) {
-  client.write('Hi!\n');
+clientServer.on('connection', function(client) {
+	client.write('hi\n');
 
-  client.on('data', function(data) {
-    console.log(data)
-  })
+	clientList.push(client)
 
+	client.on('data', function(data) {
+		for (var i = 0; i < clientList.length; i++) {
+		clientList[i].write(data)
+		}
+	})
 })
-
-chatServer.listen(9000)
