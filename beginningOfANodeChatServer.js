5a98bae1588b9d5dc24f734f162f690b07c83a44
brennangw <brennangoffwallace@gmail.com>
2014-05-25 17:47:50 -0500

    Improved messenging

--- beginningOfANodeChatServer.js
+++ beginningOfANodeChatServer.js
@@ -1,18 +1,25 @@
 var net = require('net')
 
-var chatServer = net.createServer()
+var chatServer = net.createServer(),
 	clientList = []
 
 chatServer.on('connection', function(client) {
-	client.write('hi\n');
+	client.name = client.remoteAddress + ':' + client.remotePort
+	client.write('Hi '+ client.name +'!\n');
 
 	clientList.push(client)
 
 	client.on('data', function(data) {
-		for (var i = 0; i < clientList.length; i++) {
-		clientList[i].write(data)
-		}
+		broadcast(data, client)
 	})
 })
+
+function broadcast(message, client) {
+	for (var i = 0; i < clientList.length; i++) {
+		if(client !== clientList[i]) {
+			clientList[i].write(client.name + " says " + message)
+		}
+	}
+}
 
 chatServer.listen(9000)
